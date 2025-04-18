a1aad585d684cad0ea0f63c0aba947aa47503a06
Dylan Ingui <78183544+Ur1QPlusOne@users.noreply.github.com>
2025-04-18 14:45:02 -0400

    Home and Context pages

--- app.py
+++ app.py
@@ -62,36 +62,78 @@ def make_shape(geo):
 
 
 
-#    -------------------------------------------   #
-###    -------------   HEADER   -------------    ###
-#    -------------------------------------------   #
+#    ------------------------------------------   #
+###    ---------   WEBSITE SETUP   ---------    ###
+#    ------------------------------------------   #
 
 st.set_page_config(layout="wide", page_title="Dylan's URP 535 Final", page_icon="🏙️")
 st.header("Understanding the State of Energy Efficiency in Chicago")
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Context", "Methods", "Data","Findings"])
 
+
+
+
+
+#    ------------------------------------------   #
+###    -----------   HOME PAGE   -----------    ###
+#    ------------------------------------------   #
 with tab1:
-    st.write("Hello!  My name is Dylan Ingui.  I am a junior at the University of Michigan studying Urban Technology with a minor in \
-            User Experience Design.  I come from a background in high performance building, and the purpose of my work is to \
-            promote and practice energy efficiency in urban contexts.  This project was created as a way to visualize if and where \
-            in Chicago energy efficiency is becoming prioritized.  The goal is to understand the energy efficient landscape of the city or lack thereof.")
+    col1, col2 = st.columns([3,5])
+    with col1:
+        st.header("Hello!   I'm Dylan Ingui.")
+        st.write("I am a junior at the University of Michigan studying Urban Technology with a minor in \
+            User Experience Design.")
+        st.write("I come from a background in high performance building, and the purpose of my work is to \
+            promote and practice energy efficiency in urban contexts.  This project was created as a way to understand and visualize \
+            the landscape of energy efficiency in Chicago.")
+    with col2:
+        st.image('/Users/dinguid/PH Accelerator Dropbox/Dylan Ingui/Mac/Desktop/_Urban Technology/URP535 - Urban Informatics/URP535_Final/Thermal_Image.jpg', width=500)
+    st.write("------------------------------------------------------------------------------")
 
+
+
+
+
+#    --------------------------------------------   #
+###    ---------   PROJECT CONTEXT   ---------    ###
+#    --------------------------------------------   #
 with tab2:
-    st.write("In America, there has been a large push towards energy efficiency in the built environment in a plethora of \
-            the country's largest cities.  New York City gives large buildings an energy limit through Local Law 97, and the \
-            city taxes large building owners that refuse to comply.  Boston's BERDO 2.0 and DC's BEPS programs were \
-            introduced to place strict emissions thresholds on certain building types.  Finally, San Francisco's \
-            Climate Action Plan focuses more on electrification and decarbonization rather than on emission caps.  \
-            The City of Chicago, however, is still in the early stages of climate legislation.")
-    
-    st.write("Despite not having any strong action plans passed in legislation yet, Chicago has indirectly created an effort \
-            to promote energy efficiency.  The city does have coalitions that are pushing for something similar to NYC's local \
-            law 97, and Chicago has been require buildings over 50,000 square feet to report on their energy consumption once every \
-            three years to receive an energy rating.")
+    col1, col2 = st.columns([3,5])
+    with col1:
+        st.image('/Users/dinguid/PH Accelerator Dropbox/Dylan Ingui/Mac/Desktop/_Urban Technology/URP535 - Urban Informatics/URP535_Final/Draft_Chicago_Energy_Rating_Placard.jpg', width=400)
+    with col2:
+        st.write("In America, there has been a large push towards energy efficiency in the built environment in a plethora of \
+                the country's largest cities.  New York City gives large buildings an energy limit through [Local Law 97](https://www.nyc.gov/site/buildings/codes/ll97-greenhouse-gas-emissions-reductions.page#:~:text=Local%20Law%2097%20allows%20for,Questions%20and%20LL97%20RECs%20Policy.),\
+                and the city taxes large building owners that refuse to comply.  Boston's [BERDO 2.0](https://www.iesve.com/discoveries/view/40249/berdo-2-0-decarbonizing-boston) \
+                and DC's [BEPS](https://buildinginnovationhub.org/resource/regulation-basics/?gad_source=1&gclid=CjwKCAjw8IfABhBXEiwAxRHlsCHfp_HKfMvclnSkz7fMnW7demaNLFQ3MlWtc43EwZUbwqz2srFZYBoCfaMQAvD_BwE) programs were \
+                introduced to place strict emissions thresholds on certain building types.  Finally, San Francisco's [Climate Action Plan](https://sfplanning.org/project/san-francisco-climate-action-plan#about) \
+                focuses more on electrification and decarbonization rather than on emission caps.  \
+                The City of Chicago, however, is still in the early stages of climate legislation.")
+        
+        st.write("Despite not having any strong action plans passed in legislation yet, Chicago has indirectly created an effort \
+                to promote energy efficiency through their [Chicago Energy Rating](https://www.chicago.gov/city/en/progs/env/ChicagoEnergyRating.html).  \
+                The program requires buildings over 50,000 square feet to report on their energy consumption once every three years to receive an energy \
+                rating.  Luckily, Chicago's Open Data Portal publicly provides their dataset on buildings with a Chicago Energy Rating, which is the base data \
+                utilized for this project.")
+    st.write("------------------------------------------------------------------------------")
 
+
+
+
+
+#    ---------------------------------------------   #
+###    -----------   METHODS PAGE   -----------    ###
+#    ---------------------------------------------   #
 with tab3:
     st.write("these are methods")
 
+
+
+
+
+#    -----------------------------------------------------   #
+###    -----------   DATA VISUALIZATION PAGE   -----------    ###
+#    -----------------------------------------------------   #
 with tab4:
     col1, col2 = st.columns([1, 2])
 
@@ -102,7 +144,6 @@ with tab4:
     #    --------------------------------------------   #
     ###    -------------   SELECTS   -------------    ###
     #    --------------------------------------------   #
-
     # Year slider
     min_year = setup_df['data_year'].min()
     max_year = setup_df['data_year'].max()
@@ -231,5 +272,12 @@ with tab4:
         # Display map in Streamlit
         st_data = st_folium(m, width=800, height=500)
 
+
+
+
+
+#    --------------------------------------------   #
+###    ----------   FINDINGS PAGE   ----------    ###
+#    --------------------------------------------   #
 with tab5:
     st.write('hello world 3')
